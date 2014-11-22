2447096de16e90cc2768b7302cb14d13976ee302
Daniel Gallagher <pdanielgallagher@gmail.com>
2014-11-22 10:29:19 -0800

    Updating copyrights

---
+++ main.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2014, Daniel Gallagher
+// Use of this source code is covered by the MIT License, the full
+// text of which can be found in the LICENSE file.
+
+package main
+
+func main() {
+	ListenAndServe()
+}
--- wiki_test.go
+++ wiki_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2014, Daniel Gallagher
+// Use of this source code is covered by the MIT License, the full
+// text of which can be found in the LICENSE file.
+
 package main
 
 import "testing"
